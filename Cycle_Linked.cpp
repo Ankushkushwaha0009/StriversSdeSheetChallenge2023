25e2615eb69129283abd9d8c716506589a091216
Ankush kushwaha <ankushkushwaha2828@gmail.com>
2023-07-02 01:30:41 +0530

    cycle linked list

---
+++ Cycle_Linked.cpp
@@ -0,0 +1,64 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+Node *firstNode(Node *head) {
+    if(head == NULL|| head -> next == NULL) return NULL ; 
+    Node* slow = head  ;
+    Node* fast = head  ;
+    Node* entryPoint = head ; 
+
+    while(fast -> next and fast -> next -> next) {
+          slow = slow -> next  ; 
+          fast = fast -> next -> next  ; 
+          if(fast == slow) {
+                while(entryPoint  != slow) {
+                      entryPoint = entryPoint -> next ; 
+                      slow = slow -> next  ; 
+                }
+                return slow ; 
+          }
+    }
+ return NULL ; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
